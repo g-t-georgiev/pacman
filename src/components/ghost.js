514aa36206e3d1ad6c2804bba58aa3c6a12d3913
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-12-31 19:39:42 +0200

    Moved scared state color toggling logic outside the class.

--- src/components/ghost.js
+++ src/components/ghost.js
@@ -33,15 +33,15 @@ export class Ghost {
     }
 
     draw() {    
-        this.context.fillStyle = this.color;
+        this.context.fillStyle = this.scared ? 'blue' : this.color;
         this.context.beginPath();
         this.context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         this.context.fill();
     }
 
-    update(dt, pause) {
-        if (!pause) {
-            this.moving = !pause;
+    update(dt, move) {
+        if (move) {
+            if (!this.moving) this.moving = true;
             this.position.x += this.velocity.x;
             this.position.y += this.velocity.y;
         }
